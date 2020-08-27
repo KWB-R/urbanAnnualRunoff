fa72d3308046699c6d9a3d3fb429c01ed1786201
Roberto <robetatis@yahoo.com>
2020-08-27 12:27:35 +0200

    updated abimo.R and created workflow.R

--- abimo.R
+++ abimo.R
@@ -1,110 +1,12 @@
-rawdir='Y:/WWT_Department/Projects/KEYS/Data-Work packages/WP2_SUW_pollution_Jinxi/_DataAnalysis/GIS'
-
-makeOverlay(rawdir='c:/kwb/KEYS/WP2_SUW_pollution_Jinxi/_DataAnalysis/GIS/',
-            rasterData='jxClass.img', 
-            subcatchmShape='ABIMO_Jinxi_5.shp',
-            overlayName='surfType')
-
-computeABIMOcolumn(rawdir='c:/kwb/KEYS/WP2_SUW_pollution_Jinxi/_DataAnalysis/GIS/', 
-                   subcatchmShape='ABIMO_Jinxi_5.shp',
-                   rasterData='jxClass.img',
-                   overlayName='surfType',
-                   targetValue=2,
-                   outDFname='probauJx.txt')
-
-
-
-# % other impervious areas = total impervious % (VG, from global data set) - %roof (PROBAU)
-subc@data$PROVGU <- ifelse((subc@data$VG - subc@data$PROBAU) > 0,
-                           subc@data$VG - subc@data$PROBAU,
-                           0)
-
-# % imperviousness streets
-subc@data$VGSTRASSE <- 100
-
-# %cover types in other imperv. areas (PROVGU)
-subc@data$BELAG1 <- 100
-subc@data$BELAG2 <- 0
-subc@data$BELAG3 <- 0
-subc@data$BELAG4 <- 0
-
-# %cover types in street areas
-subc@data$STR_BELAG1 <- 100
-subc@data$STR_BELAG2 <- 0
-subc@data$STR_BELAG3 <- 0
-subc@data$STR_BELAG4 <- 0
-
-# identifiers
-#subc@data$CODE <- formatC(x=subc@data$BSM, format='d', width = 5, flag = '0')
-subc@data$BEZIRK <- 1
-subc@data$STAGEB <- 1
-subc@data$BLOCK <- 1
-subc@data$TEILBLOCK <- 1
-subc@data$NUTZUNG <- 21
-subc@data$TYP <- 21
-
-subc@data$REGENJA <- 800
-subc@data$REGENSO <- 500
-
-subc@data$KANAL<- 1
-subc@data$KAN_BEB<- 100
-subc@data$KAN_VGU<- 100
-subc@data$KAN_STR<- 100
-
-subc@data$FELD_30 <- 15
-subc@data$FELD_150 <- 15
-subc@data$FLUR <- 1
-
-# select required columns and write out new shapefile
-requiredCols <- c('CODE', 'BEZIRK', 'STAGEB', 'BLOCK', 'TEILBLOCK','NUTZUNG', 'TYP',
-                  'FLGES', 'STR_FLGES',
-                  'PROBAU', 'PROVGU', 'VGSTRASSE',
-                  'BELAG1', 'BELAG2', 'BELAG3', 'BELAG4',
-                  'STR_BELAG1', 'STR_BELAG2', 'STR_BELAG3', 'STR_BELAG4',
-                  'KAN_BEB', 'KAN_VGU', 'KAN_STR',
-                  'REGENJA', 'REGENSO',
-                  'FELD_30', 'FELD_150', 'FLUR')
-subc@data <- subc@data[, requiredCols]
-
-# # change decimal separator to point
-subc@data <- as.data.frame(apply(X=apply(X=subc@data,
-                                         c(1, 2),
-                                         FUN=as.character),
-                                 c(1, 2),
-                                 FUN=gsub,
-                                 pattern="\\.",
-                                 replacement=","),
-                           stringsAsFactors = FALSE)
-
-# write ABIMO input table
-raster::shapefile(x=subc, filename='../ABIMO/ABIMO_Jinxi_v1.shp', overwrite=TRUE)
-
-
-setwd('c:/kwb/KEYS/WP2_SUW_pollution_Jinxi/_DataHIT/')
-soil <- readxl::read_xls(path='translate_soil_data.xls',
-                         sheet = "111",
-                         skip = 1,
-                         col_names=T,
-                         na=c("","na"),
-                         trim_ws = TRUE,
-                         .name_repair = "minimal")
-
-soil$`Irrigated Conditions`
-soil$Drainage
-t(t(unique(soil$`Texture Configuration`)))
-"轻壤" -> 'light soil'
-"中壤" -> 'middle soil'
-"重壤" -> 'heavy earth'
-"砂质轻壤" -> 'sandy light soil'
-"粘质中壤" -> 'clay medium soil'
-"砂质中壤" -> 'sandy medium soil'
-"砂壤" -> 'sand soil'
-"轻粘土" -> 'light clay'
-
-# spatial overlay of subcatchments and raster holding information required by ABIMO -----
+# spatial overlay of subcatchments and raster holding information required by ABIMO
 makeOverlay <- function(rawdir, rasterData, subcatchmShape, overlayName){
   
-  require(raster)
+  # This function ca be used to overlay subcatchment polygons (shapefile) with two
+  # types of raster datasets 
+  # - imperviousness: http://data.ess.tsinghua.edu.cn/fromglc10_2017v01.html
+  # - classified image containing roof and street pixels
+  
+  library(raster)
   
   setwd(rawdir)
   
@@ -119,35 +21,110 @@ makeOverlay <- function(rawdir, rasterData, subcatchmShape, overlayName){
   cat('\ndone\n')
 }
 
-# compute area and %coverage of specific cover type in each subcatchment -----------------
-computeABIMOcolumn <- function(rawdir, subcatchmShape, rasterData,
-                               overlayName, targetValue, outDFname){
+# compute ABIMO variables, either PROBAU (%roof), VG (%impervious) or STR_FLGES (street
+# area) in each subcatchment based on existing overlay
+computeABIMOvariable <- function(rawdir, subcatchmShape, rasterData,
+                                 overlayName, targetValue, outDFname,
+                                 mask, street){
+  
+  # This function can be used to generate the data required for ABIMO columns:
+  # - VG: % imperviousness
+  # - PROBAU: % roof area
+  # - STR_FLGES: street area
+  # It reads overlay objects produced by makeOverlay and uses pixel sizes to compute
+  # the corresponding areas
+  
+  # Street areas are allocated to each subcatchment based on their size. Since there
+  # may be internal and external street area (if the subcatchment polygons have gaps
+  # between them), the function first computes the internal street area and then 
+  # allocates the external street area to each subcatchment based on its size (as is
+  # done in Berlin)
+  
+  library(raster)
   
   setwd(rawdir)
-
+  
   # load data  
+  cat('\nreading mask, subcatchments and raster data...')
   subc <- raster::shapefile(subcatchmShape, stringsAsFactors=FALSE)
   surf <- raster::raster(rasterData)
+  mask <- raster::shapefile(mask)
   
-  # grab overlay object
-  cat('\nreading existing overlay...')
-  load(paste0(overlayName, '.Rdata'), envir=.GlobalEnv)
+  # change decimal separator from comma to point in subcatchment data and convert to
+  # numbers
+  subc@data <- as.data.frame(apply(X=apply(X=subc@data,
+                                           c(1, 2),
+                                           FUN=gsub,
+                                           pattern=',',
+                                           replacement='.'),
+                                   c(1, 2),
+                                   FUN=as.numeric),
+                             stringsAsFactors = FALSE)
+  
+  # pad CODE in in subcatchment data with zeroes
+  nch <- max(nchar(subc@data$CODE))
+  subc@data$CODE <- formatC(x=subc@data$CODE,
+                            width=nch+1,
+                            flag=0)
   cat('\ndone\n')
   
-  # compute cell area
+  # grab overlay object
+  cat('\nreading overlay object...')
+  load(paste0(overlayName, '.Rdata'))
+  cat('\ndone\n')
+  
+  # compute raster cell area
   rasterRes <- raster::res(surf)
   cellsize <- rasterRes[1]*rasterRes[2]
   
-  # count pixels with target cover type (target value)
-  count <- sapply(X=get(overlayName, envir=.GlobalEnv), FUN=function(a) sum(a==targetValue))
-  ncellsubc <- sapply(X=get(overlayName, envir=.GlobalEnv), FUN=length)
-  
-  # make output data.frame
-  cat('\nbuilding output data.frame')
-  out <- data.frame(CODE=subc@data$CODE,
-                    area=cellsize*count,
-                    perc_cover=100*count/ncellsubc)
-  cat('\ndone\n')
+  # routine for street area
+  if(street){
+    
+    # as is done for Berlin, street area outside of the subcatchment polygons is distributed
+    # among the polygons in proportion to their area. thus: 
+    # street area of polygon = internal street area + allocated external street area 
+    
+    # make cells outside mask = NA
+    cat('\nmasking cells outside study area...')
+    surf2 <- raster::mask(surf, mask)
+    cat('\ndone\n')
+    
+    # street area within subcatchment polygons
+    streetAreaIn <- sapply(X=get(overlayName), 
+                           FUN=function(a) sum(a==targetValue)*cellsize)
+    
+    # total street area in study area
+    sa <- surf2[surf2==targetValue]
+    sa <- sa[!is.na(sa)]
+    streetAreaTot <- length(sa)*cellsize
+    
+    # street area outside subcatchment polygons = streetAreaTot - streetAreaIn
+    streetAreaOut <- streetAreaTot - sum(streetAreaIn)
+    
+    # distribute streetAreaOut among subcatchments in proportion to their area
+    STR_FLGES <- formatC(streetAreaIn + 
+                           streetAreaOut*subc@data$FLGES/sum(subc@data$FLGES),
+                         format='d')
+    
+    # make output data.frame
+    cat('\nbuilding output data.frame')
+    out <- data.frame(CODE=subc@data$CODE,
+                      STR_FLGES=STR_FLGES)
+    cat('\ndone\n')
+    
+  } else {
+    
+    # count pixels with target cover type (target value)
+    count <- sapply(X=get(overlayName), FUN=function(a) sum(a==targetValue))
+    ncellsubc <- sapply(X=get(overlayName), FUN=length)
+    
+    # make output data.frame
+    cat('\nbuilding output data.frame')
+    out <- data.frame(CODE=subc@data$CODE,
+                      area=cellsize*count,
+                      perc_cover=100*count/ncellsubc)
+    cat('\ndone\n')
+  }
   
   # write output file
   cat('\nwriting output data.frame')
@@ -155,65 +132,9 @@ computeABIMOcolumn <- function(rawdir, subcatchmShape, rasterData,
   cat('\ndone\n')
 }
 
-# compute area  of street areas ----------------------------------------------------------
-makeABIMOstreet <- function(rawdir, sourceRaster, targetValue, 
-                            subcatchmShape, overlayName, outDFname,
-                            existingOverlay){
-  
-  # as is done for Berlin, street area outside of the subcatchment polygons is distributed
-  # among the polygons in proportion to their area. thus: 
-  # street area of polygon = internal street area + allocated external street area 
-
-  require(raster)
-  
-  setwd(rawdir)
-  
-  # load data  
-  subc <- raster::shapefile(subcatchmShape, stringsAsFactors=FALSE)
-  surf <- raster::raster(sourceRaster)
-  
-  
-  cat('\nreading existing overlay...')
-  load(overlayName)
-  cat('\ndone\n')
-  
-  
-  
-  cat('\noverlaying data...')
-  ov <- raster::extract(x=surf, y=subc)
-  cat('\nsaving overlay...')
-  save(ov, file=overlayName)
-  cat('\ndone\n')
-  
-  
-  
-  # street area within subcatchment polygons
-  streetAreaIn <- sapply(X=ov,
-                           FUN=function(a){
-                             length(a[a==4])}) # 4 = street
-  
-  # total street area in study area
-  streetAreaTot <- length(surf[surf==4])
-  
-  # difference = street area outside subcatchment polygons
-  streetAreaOut <- streetAreaTot - sum(streetAreaIn)
-  
-  
-  subc@data$STR_FLGES <- formatC(streetAreaIn + 
-                                   streetAreaOut*subc@data$FLGES/sum(subc@data$FLGES),
-                                 format = 'd')
-  
-  
-  
-  
-  
-}
-
-
-
-
-# read dbf results file, join with input shapefile and write final table -----------------
-postProcessABIMO <- function(rawdir, ABIMOin, ABIMOout, ABIMOjoinedName){
+# read dbf results file, join with input shapefile and write output shapefile including
+# ABIMO results
+postProcessABIMO <- function(rawdir, nameABIMOin, nameABIMOout, ABIMOjoinedName){
 
   library(dplyr)
   library(raster)
@@ -222,8 +143,23 @@ postProcessABIMO <- function(rawdir, ABIMOin, ABIMOout, ABIMOjoinedName){
   setwd(rawdir)
   
   # read files
-  ABIMOin <- raster::shapefile(x=ABIMOin, stringsAsFactors=FALSE)
-  ABIMOout <- foreign::read.dbf(file=ABIMOout, as.is=TRUE)
+  ABIMOin <- raster::shapefile(x=nameABIMOin, stringsAsFactors=FALSE)
+  ABIMOout <- foreign::read.dbf(file=nameABIMOout, as.is=TRUE)
+  
+  # change decimal separator from comma to point
+  ABIMOin@data <- as.data.frame(apply(X=apply(X=ABIMOin@data,
+                                              c(1, 2),
+                                              FUN=gsub,
+                                              pattern=',',
+                                              replacement='.'),
+                                      c(1, 2),
+                                      FUN=as.numeric),
+                                stringsAsFactors = FALSE)
+  
+  # pad CODE in ABIMOin with zeroes to match CODE in ABIMOout
+  ABIMOin$CODE <- formatC(x=ABIMOin$CODE, 
+                          width=nchar(ABIMOout$CODE)[1],
+                          flag=0)
   
   # join 
   ABIMOjoined <- ABIMOin
@@ -233,6 +169,4 @@ postProcessABIMO <- function(rawdir, ABIMOin, ABIMOout, ABIMOjoinedName){
   # write out joined table output as shapefile
   raster::shapefile(x=ABIMOjoined, filename=ABIMOjoinedName, overwrite=TRUE)
 }
-
-
 
---
+++ workflow.R
@@ -0,0 +1,135 @@
+# workflow:
+# step 1: build classification model
+# step 2: classify image for roofs and streets
+# step 3: make overlays for total impervious area, roof and street for each subcatchment
+# step 4: compute ABIMO variables PROBAU (%roof), VG (%impervious) and STR_FLGES
+# step 5: use raw code to compute and allocate PROVGU and all other ABIMO variables
+# step 6: go to ABIMO and run (could we call it from R?)
+# step 7: post-process ABIMO output file -> join it with input shape file for visualization
+#         in GIS
+
+#rawdir='Y:/WWT_Department/Projects/KEYS/Data-Work packages/WP2_SUW_pollution_Jinxi/_DataAnalysis/GIS'
+#rawdir='c:/kwb/KEYS/WP2_SUW_pollution_Jinxi/_DataAnalysis/GIS/'
+
+buildClassMod(dataPath='Y:/WWT_Department/Projects/KEYS/Data-Work packages/WP2_SUW_pollution_Beijing/_DataAnalysis/GIS/',
+              image='tz.tif',
+              # column name of surface type in groundTruth must be 'cover'
+              groundTruth='groundtruth2.shp', 
+              spectrSigName='spectrSigTz.Rdata',
+              modelName='rForestTz.Rdata',
+              nCores=5)
+
+predictSurfClass(dataPath='Y:/WWT_Department/Projects/KEYS/Data-Work packages/WP2_SUW_pollution_Beijing/_DataAnalysis/GIS/',
+                 modelName='rForestTz.Rdata',
+                 image='tz.tif',
+                 predName='tzClass.img')
+
+makeOverlay(rawdir='c:/kwb/KEYS/WP2_SUW_pollution_Jinxi/_DataAnalysis/GIS/',
+            rasterData='jxClass.img', 
+            subcatchmShape='ABIMO_Jinxi_5.shp',
+            overlayName='surfType')
+
+# 2=roof, 80=impervious (in another raster dataset), 4 = street
+computeABIMOvariable(rawdir='c:/kwb/KEYS/WP2_SUW_pollution_Jinxi/_DataAnalysis/GIS/',
+                     subcatchmShape='ABIMO_Jinxi_5.shp',
+                     mask='maskUrbanCore.shp',
+                     rasterData='imperv.tif',
+                     overlayName='imperv',
+                     targetValue=80,
+                     outDFname='impervJx.txt',
+                     street=FALSE)
+
+postProcessABIMO(rawdir='c:/kwb/KEYS/WP2_SUW_pollution_Jinxi/_DataAnalysis/ABIMO/',
+                 nameABIMOin='ABIMO_Jinxi_v1.shp',
+                 nameABIMOout='ABIMO_Jinxi_v1out.dbf',
+                 ABIMOjoinedName='ABIMO_Jinxi_v1outJoined.dbf')
+
+
+# raw code ------------------------------------------------------------------------------
+
+# % other impervious areas = total impervious % (VG, from global data set) 
+# - %roof (PROBAU)
+subc@data$PROVGU <- ifelse((subc@data$VG - subc@data$PROBAU) > 0,
+                           subc@data$VG - subc@data$PROBAU,
+                           0)
+
+# % imperviousness streets
+subc@data$VGSTRASSE <- 100
+
+# %cover types in other imperv. areas (PROVGU)
+subc@data$BELAG1 <- 100
+subc@data$BELAG2 <- 0
+subc@data$BELAG3 <- 0
+subc@data$BELAG4 <- 0
+
+# %cover types in street areas
+subc@data$STR_BELAG1 <- 100
+subc@data$STR_BELAG2 <- 0
+subc@data$STR_BELAG3 <- 0
+subc@data$STR_BELAG4 <- 0
+
+# identifiers
+subc@data$BEZIRK <- 1
+subc@data$STAGEB <- 1
+subc@data$BLOCK <- 1
+subc@data$TEILBLOCK <- 1
+subc@data$NUTZUNG <- 21
+subc@data$TYP <- 21
+
+subc@data$REGENJA <- 800
+subc@data$REGENSO <- 500
+
+subc@data$KANAL<- 1
+subc@data$KAN_BEB<- 100
+subc@data$KAN_VGU<- 100
+subc@data$KAN_STR<- 100
+
+subc@data$FELD_30 <- 15
+subc@data$FELD_150 <- 15
+subc@data$FLUR <- 1
+
+# select required columns and write out new shapefile
+requiredCols <- c('CODE', 'BEZIRK', 'STAGEB', 'BLOCK', 'TEILBLOCK','NUTZUNG', 'TYP',
+                  'FLGES', 'STR_FLGES',
+                  'PROBAU', 'PROVGU', 'VGSTRASSE',
+                  'BELAG1', 'BELAG2', 'BELAG3', 'BELAG4',
+                  'STR_BELAG1', 'STR_BELAG2', 'STR_BELAG3', 'STR_BELAG4',
+                  'KAN_BEB', 'KAN_VGU', 'KAN_STR',
+                  'REGENJA', 'REGENSO',
+                  'FELD_30', 'FELD_150', 'FLUR')
+subc@data <- subc@data[, requiredCols]
+
+# # change decimal separator to point
+subc@data <- as.data.frame(apply(X=apply(X=subc@data,
+                                         c(1, 2),
+                                         FUN=as.character),
+                                 c(1, 2),
+                                 FUN=gsub,
+                                 pattern="\\.",
+                                 replacement=","),
+                           stringsAsFactors = FALSE)
+
+# write ABIMO input table
+raster::shapefile(x=subc, filename='../ABIMO/ABIMO_Jinxi_v1.shp', overwrite=TRUE)
+
+
+setwd('c:/kwb/KEYS/WP2_SUW_pollution_Jinxi/_DataHIT/')
+soil <- readxl::read_xls(path='translate_soil_data.xls',
+                         sheet = "111",
+                         skip = 1,
+                         col_names=T,
+                         na=c("","na"),
+                         trim_ws = TRUE,
+                         .name_repair = "minimal")
+
+soil$`Irrigated Conditions`
+soil$Drainage
+t(t(unique(soil$`Texture Configuration`)))
+# "轻壤" -> 'light soil'
+# "中壤" -> 'middle soil'
+# "重壤" -> 'heavy earth'
+# "砂质轻壤" -> 'sandy light soil'
+# "粘质中壤" -> 'clay medium soil'
+# "砂质中壤" -> 'sandy medium soil'
+# "砂壤" -> 'sand soil'
+# "轻粘土" -> 'light clay'
