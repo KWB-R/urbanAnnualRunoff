8b5e60dd9df8aa4aa278ff7fdadd1f710da622e0
Roberto Tatis-Muvdi <rtatmuv@KWB.BERLIN>
2020-12-04 16:21:28 +0100

    enhance documentation imgClass.R

--- R/imgClass.R
+++ R/imgClass.R
@@ -1,4 +1,4 @@
-# image segmentation with random forest based on spectral signatures
+# image segmentation for urban surfaces with random forest based on spectral signatures
 
 #' build image classification model (random forest)
 #' @description Wrapper function for fitting a random forest using a multi-band
@@ -6,34 +6,28 @@
 #' (green areas, water surface), etc. These categories are defined by the user
 #' in the ground truth data. Training and testing are done using repeated
 #' cross-validation with package caret
-#' @param rawdir path to directory containing the image to be classified and the
+#' @param rawdir Path to directory containing the image to be classified and the
 #' ground truth data.
 #' @param image The image to be classified. Supported formats are given in the
 #' raster package's brick function.
-#' @param groundTruth shapefile containing polygons indicating the observed
+#' @param groundTruth Shapefile containing polygons indicating the observed
 #' classes of a sample of pixels. These classes must be contained in a column
 #' named 'cover' in the shape-file's attribute table. The table may contain
 #' further columns.
-#' @param groundTruthValues list with key value pairs (default: list('roof' = 1,
-#' 'street' = 2, 'pervious' = 3, 'shadow' = 4, 'water' = 5))
-#' @param overlayExists if FALSE, the function overlays the ground truth data
+#' @param groundTruthValues List with key value pairs giving a numerical ID for
+#' each surface type (default: list('roof' = 1, 'street' = 2, 'pervious' = 3,
+#' shadow' = 4, 'water' = 5))
+#' @param overlayExists If FALSE, the function overlays the ground truth data
 #' and the image (time consuming) and saves an R object containing the former's
 #' spectral signatures with name spectrSigName (overlay object). If TRUE, the
 #' function will skip this overlay operation and read an existing overlay object
-#' with name 'spectrSigName'.(default: FALSE)
+#' with name 'spectrSigName'. (default: FALSE)
 #' @param spectrSigName File name of overlay object, either for saving a new or
 #' load an existing file.
 #' @param modelName File name for saving the fitted random forest model
-#' @param nCores  no. of cores for running in parallel mode (uses library
+#' @param nCores  No. of cores for running in parallel mode (uses library
 #' 'doParallel'), (default: parallel::detectCores() - 1)
-#' @param mtryGrd Number of trees to grow. In the random forests literature, this
-#' is referred to as the ntree parameter. Larger number of trees produce more
-#' stable models and covariate importance estimates, but require more memory and
-#' a longer run time. For small datasets, 50 trees may be sufficient. For larger
-#' datasets, 500 or more may be required. Please consult the random forests
-#' literature for extensive discussion of this parameter (e.g. Cutler et al., 2007;
-#' Strobl et al., 2007; Strobl et al., 2008).
-#' @param ntreeGrd Number of variables available for splitting at each tree node.
+#' @param mtryGrd Number of variables available for splitting at each tree node.
 #' In the random forests literature, this is referred to as the mtry parameter.
 #' There is extensive discussion in the literature about the influence of mtry.
 #' Cutler et al. (2007) reported that different values of mtry did not affect
@@ -41,13 +35,22 @@
 #' metrics (sensitivity, specificity, kappa, and ROC AUC) were stable under
 #' different values of mtry. On the other hand, Strobl et al. (2008) reported
 #' that mtry had a strong influence on predictor variable importance estimates.
-#' @param nfolds number of folds in repeated cross validation (caret),
+#' @param ntreeGrd Number of trees to grow. In the random forests literature, this
+#' is referred to as the ntree parameter. Larger number of trees produce more
+#' stable models and covariate importance estimates, but require more memory and
+#' a longer run time. For small datasets, 50 trees may be sufficient. For larger
+#' datasets, 500 or more may be required. Please consult the random forests
+#' literature for extensive discussion of this parameter (e.g. Cutler et al., 2007;
+#' Strobl et al., 2007; Strobl et al., 2008).
+#' @param nfolds Number of folds in repeated cross validation (caret),
 #' (default: 3)
-#' @param nodesize a single value (not included in grid search), (default: 3)
-#' @param cvrepeats number of repeats in repeated cross validation (caret),
+#' @param nodesize Minimum size of terminal nodes. (not included
+#' in grid search), (default: 3) (from randomForest)
+#' @param cvrepeats Number of repeats in repeated cross validation (caret),
 #' (default: 2)
 #'
-#' @return writes a lot of output files to different folders ??? (details!)
+#' @return Writes spectralSignatures (if overlayExists is FALSE) and fitted random
+#' forest model with name modelName.
 #' @export
 #' @importFrom caret createFolds train trainControl
 #' @importFrom doParallel registerDoParallel
@@ -148,12 +151,12 @@ buildClassMod <- function(rawdir,
 # apply model to predict surface type (roof, street, ...)
 #' apply model to predict surface type (roof, street, ...)
 #'
-#' @param rawdir path to raw data directory
-#' @param modelName modelName
-#' @param image image
-#' @param predName name of prediction file
+#' @param rawdir Path to raw data directory
+#' @param modelName Name of fitted random forest model saved by buildClassMod
+#' @param image Image to be classified
+#' @param predName Name of output raster file (classified image)
 #'
-#' @return write raster in "rawdir" with file name "predName"
+#' @return Writes raster in "rawdir" with file name "predName"
 #' @export
 #' @importFrom raster brick predict writeRaster
 #'
